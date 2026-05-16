190d89d6f5792c8e8a071b7b3a52a0574eac5f54
mazuzehlayisani <mazuzehlayisani@gmail.com>
2026-05-16 19:50:46 +0200

    Lesson 1 changes

---
+++ Lesson 1.py
@@ -0,0 +1,13 @@
+# User 1
+name1 = input("Enter your name: ")
+age1 = int(input("Enter your age: "))
+height1 = float(input("Enter your height: "))
+
+# User 2
+name2 = input("Enter your name: ")
+age2 = int(input("Enter your age: "))
+height2 = float(input("Enter your height: "))
+
+print(f"{name1}: age {age1}, height {height1} cm")
+print(f"{name2}: age {age2}, height {height2} cm")
+print(f"(add):{height1 + height2}")
